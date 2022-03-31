9ad90f8e3153548f4b62fac3012ae4537fda42c1
callumjr <callumrosier.dev@gmail.com>
2022-03-31 17:46:36 +0100

    First commit

---
+++ stage_name_gen.py
@@ -0,0 +1,6 @@
+print('Welcome to the Stage Name Generator')
+
+adjective = input('What adjective best describes you?')
+pet = input('What is the name of your pet?')
+
+print('Your stage name could be ' + adjective + ' ' + pet)
